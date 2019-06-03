95a9d8a8adef400f89af0bdeaa7253f484389366
Jeff Simons Decena <rjdecena@multisyscorp.com>
2019-06-03 12:25:19 +0800

    Create a sample unit test

---
+++ tests/TestCase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Geekhives\BaseRepository\Tests;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    use WithFaker, WithoutMiddleware;
+
+    /**
+     * Set up
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('migrate')->run();
+
+        $this->withFactories(__DIR__ . '/database/factories');
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        //
+    }
+
+    /**
+     * @param Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return ['Geekhives\BaseRepository\ServiceProvider'];
+    }
+}
---
+++ tests/Unit/ExampleUnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Geekhives\BaseRepository\Tests\Unit;
+
+use Geekhives\BaseRepository\Tests\TestCase;
+
+class ExampleUnitTest extends TestCase
+{
+    /** @test */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
